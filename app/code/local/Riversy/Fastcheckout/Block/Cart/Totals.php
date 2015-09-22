659845a32c4109f7e750b1b959783fe3396f2f5f
Igor Goltsov <riversy@gmail.com>
2015-09-23 00:57:02 +0300

    Update # Local and Other forms were sorted out

--- app/code/local/Riversy/Fastcheckout/Block/Cart/Totals.php
+++ app/code/local/Riversy/Fastcheckout/Block/Cart/Totals.php
@@ -51,12 +51,19 @@ class Riversy_Fastcheckout_Block_Cart_Totals extends Mage_Checkout_Block_Cart_To
     protected function _getTotalRenderer($code)
     {
         if (!isset($this->_totalRenderers[$code])) {
+
             $this->_totalRenderers[$code] = $this->_defaultRenderer;
             $config = Mage::getConfig()->getNode("global/sales/quote/totals/{$code}/renderer");
-            if ($config)
-                $this->_totalRenderers[$code] = (string) $config;
-
-            $this->_totalRenderers[$code] = $this->getLayout()->createBlock($this->_totalRenderers[$code], "{$code}_total_renderer");
+            if ($config) {
+                $this->_totalRenderers[$code] = (string)$config;
+            }
+            $this->_totalRenderers[$code] = $this
+                ->getLayout()
+                ->createBlock(
+                    $this->_totalRenderers[$code],
+                    "{$code}_total_renderer"
+                )
+            ;
         }
 
         return $this->_totalRenderers[$code];
